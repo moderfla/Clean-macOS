069c067c3e1258389225728f593f93c440d52891
Alfredo M <58049346+moderfla@users.noreply.github.com>
2021-12-07 09:13:03 -0300

    Update config.sh

--- bin/config.sh
+++ bin/config.sh
@@ -97,11 +97,11 @@ cp $CONFIG/.gitconfig ~/.gitconfig
 
 # Configure macOS Finder
 printf "⚙️ Configure Finder...\n"
-defaults write com.apple.finder AppleShowAllFiles true
-defaults write com.apple.finder _FXShowPosixPathInTitle -bool YES
-defaults write com.apple.finder ShowPathbar -bool true
+# defaults write com.apple.finder AppleShowAllFiles true
+# defaults write com.apple.finder _FXShowPosixPathInTitle -bool YES
+# defaults write com.apple.finder ShowPathbar -bool true
 chflags nohidden ~/Library
-/System/Library/Frameworks/CoreServices.framework/Frameworks/LaunchServices.framework/Support/lsregister -kill -r -domain local -domain system -domain user
+# /System/Library/Frameworks/CoreServices.framework/Frameworks/LaunchServices.framework/Support/lsregister -kill -r -domain local -domain system -domain user
 
 # Configure macOS Screen Capture
 printf "⚙️ Save screenshots in PNG format...\n"
@@ -111,46 +111,46 @@ defaults write com.apple.screencapture type -string "png"
 
 # Configure macOS Keyboard
 printf "⚙️ Configure Keyboard...\n"
-defaults write -g NSAutomaticSpellingCorrectionEnabled -bool false
-defaults write -g NSAutomaticPeriodSubstitutionEnabled -bool false
-defaults write -g NSAutomaticDashSubstitutionEnabled -bool false
-defaults write -g NSAutomaticQuoteSubstitutionEnabled -bool false
-defaults write NSGlobalDomain KeyRepeat -int 1
-defaults write NSGlobalDomain InitialKeyRepeat -int 10
-defaults write com.apple.messageshelper.MessageController SOInputLineSettings -dict-add "continuousSpellCheckingEnabled" -bool false
+# defaults write -g NSAutomaticSpellingCorrectionEnabled -bool false
+# defaults write -g NSAutomaticPeriodSubstitutionEnabled -bool false
+# defaults write -g NSAutomaticDashSubstitutionEnabled -bool false
+# defaults write -g NSAutomaticQuoteSubstitutionEnabled -bool false
+# defaults write NSGlobalDomain KeyRepeat -int 1
+# defaults write NSGlobalDomain InitialKeyRepeat -int 10
+# defaults write com.apple.messageshelper.MessageController SOInputLineSettings -dict-add "continuousSpellCheckingEnabled" -bool false
 
 # Configure macOS Safari
 printf "⚙️ Configure Safari...\n"
-defaults write com.apple.Safari UniversalSearchEnabled -bool false
-defaults write com.apple.Safari SuppressSearchSuggestions -bool true
-defaults write com.apple.Safari WebAutomaticSpellingCorrectionEnabled -bool false
-defaults write com.apple.Safari AutoOpenSafeDownloads -bool false
-defaults write com.apple.Safari ShowFavoritesBar -bool true
+# defaults write com.apple.Safari UniversalSearchEnabled -bool false
+# defaults write com.apple.Safari SuppressSearchSuggestions -bool true
+# defaults write com.apple.Safari WebAutomaticSpellingCorrectionEnabled -bool false
+# defaults write com.apple.Safari AutoOpenSafeDownloads -bool false
+# defaults write com.apple.Safari ShowFavoritesBar -bool true
 defaults write com.apple.Safari IncludeDevelopMenu -bool true
 
 # Configure macOS TextEdit
-printf "⚙️ Configure TextEdit...\n"
-defaults write com.apple.TextEdit RichText -int 0
-defaults write com.apple.TextEdit PlainTextEncoding -int 4
-defaults write com.apple.TextEdit PlainTextEncodingForWrite -int 4
+# printf "⚙️ Configure TextEdit...\n"
+# defaults write com.apple.TextEdit RichText -int 0
+# defaults write com.apple.TextEdit PlainTextEncoding -int 4
+# defaults write com.apple.TextEdit PlainTextEncodingForWrite -int 4
 
 # Configure macOS Trackpad
-printf "⚙️ Configure Trackpad...\n"
-defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad Clicking -bool true
-defaults -currentHost write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
-defaults write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
-defaults write NSGlobalDomain com.apple.swipescrolldirection -bool false
+# printf "⚙️ Configure Trackpad...\n"
+# defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad Clicking -bool true
+# defaults -currentHost write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
+# defaults write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
+# defaults write NSGlobalDomain com.apple.swipescrolldirection -bool false
 
 # Configure macOS
 printf "⚙️ Various configuration...\n"
-defaults write com.apple.gamed Disabled -bool true
-sudo /usr/libexec/ApplicationFirewall/socketfilterfw --setglobalstate on
-defaults write com.apple.TimeMachine DoNotOfferNewDisksForBackup -bool true
+# defaults write com.apple.gamed Disabled -bool true
+# sudo /usr/libexec/ApplicationFirewall/socketfilterfw --setglobalstate on
+# defaults write com.apple.TimeMachine DoNotOfferNewDisksForBackup -bool true
 
 # Create Projects directory
-printf "⚙️ Create Projects directory...\n"
-mkdir ${HOME}/Projects
-chmod 777 ${HOME}/Projects
+# printf "⚙️ Create Projects directory...\n"
+# mkdir ${HOME}/Projects
+# chmod 777 ${HOME}/Projects
 
 # Check if Python3 is installed via Homebrew
 #if brew ls --versions python3 > /dev/null; then
